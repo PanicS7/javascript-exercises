69a6431b13f6fdbe40de69048c9129871d27e12f
Sanja <gamesg274@gmail.com>
2022-06-13 23:30:43 +0200

    Completed 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+  if (num < 0) {
+    return "ERROR";
+  }
 
+  let output = string.repeat(num);
+  return output;
 };
 
 // Do not edit below this line
