0bd45897ca173483c2f364a1c203c8245a8d179c
Sanja <gamesg274@gmail.com>
2022-06-14 15:14:17 +0200

    Completed 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...item) {
+ let arrayCopy = [...array];
 
+ item.map(i => {
+  arrayCopy.indexOf(i) !== -1 && arrayCopy.splice(arrayCopy.indexOf(i), 1);
+ })
+
+ return arrayCopy;
 };
 
 // Do not edit below this line
