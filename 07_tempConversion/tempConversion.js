cd697f90b39e16181f346ae3324a3def2deaf7f1
Sanja <gamesg274@gmail.com>
2022-06-14 16:29:05 +0200

    Completed 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,21 @@
-const ftoc = function() {
-
+const ftoc = function (fahrenheit) {
+  // formula C = (F-32) * (5/9);
+  let celsiusValue = (fahrenheit - 32) * (5 / 9);
+  return Number.isInteger(celsiusValue)
+    ? celsiusValue
+    : parseFloat(celsiusValue.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function (celsius) {
+  // formula F = C * (9/5) + 32
+  let fahrenheitValue = celsius * (9 / 5) + 32;
+  return Number.isInteger(fahrenheitValue)
+    ? fahrenheitValue
+    : parseFloat(fahrenheitValue.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
