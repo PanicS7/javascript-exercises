2459c316438940bd9f72a997c33ed2b73ac9bdb6
Sanja <gamesg274@gmail.com>
2022-06-14 15:29:52 +0200

    Completed 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  let result = 0;
 
+  if (
+    typeof num1 === "number" &&
+    num1 > 0 &&
+    typeof num2 === "number" &&
+    num2 > 0
+  ) {
+    let largerNum = num1 > num2 ? num1 : num2;
+    let smallerNum = num1 < num2 ? num1 : num2;
+
+    for (let i = smallerNum; i <= largerNum; i++) {
+      result += i;
+    }
+    return result;
+  }
+
+  return "ERROR";
 };
 
 // Do not edit below this line
