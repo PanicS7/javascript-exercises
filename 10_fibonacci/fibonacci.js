1dde8e7e6d2ed4bd5759011b0c7ef74e0c4a1abf
Sanja <gamesg274@gmail.com>
2022-06-14 22:37:37 +0200

    Completed 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  let number = parseInt(num);
+  if (number <= 0) {
+    return "OOPS";
+  }
 
+  let fibonacciSeq = [];
+  let counter = 0;
+
+  while (fibonacciSeq.length !== number) {
+    if (counter > 1) {
+      fibonacciSeq.push(fibonacciSeq[counter - 2] + fibonacciSeq[counter - 1]);
+    }
+    else {
+      fibonacciSeq.push(1);
+    }
+    counter++;
+  }
+  
+  return fibonacciSeq[number - 1];
 };
 
 // Do not edit below this line
