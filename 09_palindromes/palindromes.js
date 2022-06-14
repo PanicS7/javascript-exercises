20b51de30b2a0719a660a757e4f20f7844b37d67
Sanja <gamesg274@gmail.com>
2022-06-14 22:05:34 +0200

    Completed 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let reversedStr = [];
+  // replace punctuation and remove whitespace
+  // without whitespace it checks string with multiple word
+  let removedPunctuation = str.replace(/[^\w\s]|_/g, "").replace(/\s+/g, "");
 
+  // reverse string
+  [...removedPunctuation].map((letter) => {
+    reversedStr.unshift(letter);
+  });
+  reversedStr = reversedStr.join("");
+
+  if (removedPunctuation.toLowerCase() === reversedStr.toLowerCase()) {
+    return true;
+  }
+
+  return false;
 };
 
 // Do not edit below this line
