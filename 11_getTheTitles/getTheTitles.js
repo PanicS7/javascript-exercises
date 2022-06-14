fff903838c3f5387fb6bdd90dbe479c7dc31b6b2
Sanja <gamesg274@gmail.com>
2022-06-14 22:43:01 +0200

    Completed 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+  let booksArr = [];
+  books.map((book) => {
+    booksArr.push(book.title);
+  });
 
+  return booksArr;
 };
 
 // Do not edit below this line
