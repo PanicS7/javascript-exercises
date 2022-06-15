82279a2b3844434a85c6c05082650bae8c92a2fc
Sanja <gamesg274@gmail.com>
2022-06-15 15:53:16 +0200

    Completed 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,30 @@
-const findTheOldest = function() {
+const findTheOldest = function (personData) {
+  let currentYear = new Date().getFullYear();
 
+  let oldestPerson = personData.reduce((prev, curr) => {
+    let prevOld;
+    if (prev.hasOwnProperty("yearOfDeath")) {
+      prevOld = prev.yearOfDeath - prev.yearOfBirth;
+    } else {
+      prevOld = currentYear - prev.yearOfBirth;
+    }
+
+    let currOld;
+    if (curr.hasOwnProperty("yearOfDeath")) {
+      currOld = curr.yearOfDeath - curr.yearOfBirth;
+    } else {
+      prevOld = currentYear - curr.yearOfBirth;
+    }
+
+    if (prevOld > currOld) {
+      return prev;
+    } else {
+      return curr;
+    }
+
+  });
+
+  return oldestPerson;
 };
 
 // Do not edit below this line
